c6ef8600c48946fbd31fb7a2045089acba3fbca0
Miguel Avila <98852302+MiguelAAvila@users.noreply.github.com>
2022-10-01 18:21:35 -0600

    added state

--- src/Pikcha.elm
+++ src/Pikcha.elm
@@ -1,24 +1,57 @@
+-- MVU Architecture (Elm Architecture)
+-- Model, View, Update
+--  Model / State
+
+
 module Pikcha exposing (main)
+
 import Html exposing (..)
 import Html.Attributes exposing (class, src)
+import Html.Events exposing (onClick)
+import Browser
 
+baseUrl : String
+baseUrl = "images/"
 
-viewPhoto : String -> String -> Html msg
-viewPhoto url caption = 
+type Msg = 
+    Like | Unlike
+
+initialModel : { url : String, caption : String, liked: Bool }
+initialModel = 
+    { 
+        url = baseUrl ++ "cats.jpg"
+        ,caption = "Cat Family"
+        ,liked = False
+    }
+
+viewPhoto : { url : String, caption : String, liked: Bool }  -> Html Msg
+viewPhoto model = 
+    let 
+        buttonType = 
+            if model.liked then "favorite" else "favorite_border"
+        msg =
+            if model.liked then Unlike else Like
+    in
+
     div [ class "detailed-photo" ]
             [ 
-                img [ src url] [],
-                div [ class "photo-info" ] 
-                [ h2 [class "caption" ] [ text caption ] ]
+                img [ src model.url ] [],
+                div [ class "photo-info" ] [
+                     div [ class "favorite-button" ]
+                [
+                    span 
+                    [ class "material-icons md-48", onClick msg]
+                    [ text buttonType ]
+                ] ], 
+                h2 [ class "caption" ] [ text model.caption ] 
             ]
 
 
 
 
+view : { url : String, caption : String, liked: Bool } -> Html Msg
 
-main : Html msg
-
-main = 
+view model = 
     -- div [ class "header" ] 
     --     [ h1 [] [ text  "Pikcha" ] ]
     div [] 
@@ -27,7 +60,25 @@ main =
         [ h1 [] [ text "Pikcha" ]],
         div [ class "content-flow" ]
         [
-            viewPhoto "images/cats.jpg" "Cat Family",
-            viewPhoto "images/elephants.jpg" "Elephants Family"
+            viewPhoto model
         ]
     ]
+
+update : Msg -> { url : String, caption : String, liked: Bool } -> { url : String, caption : String, liked: Bool }
+
+update msg model =
+    case msg of 
+        Like -> 
+            { model | liked = True }
+        Unlike -> 
+            { model | liked = False }
+
+
+main : Program () { url : String, caption : String, liked: Bool } Msg
+main = 
+    Browser.sandbox
+    {
+        init = initialModel,
+        view = view,
+        update = update
+    }
